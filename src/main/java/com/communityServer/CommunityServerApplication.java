4e4363ac1a9249e3e923b46be74ce18c6ae11760
Devendra sahu <devendrasahu0309@gmail.com>
2022-09-06 08:11:23 +0530

    community server first commit

---
+++ src/main/java/com/communityServer/CommunityServerApplication.java
@@ -0,0 +1,13 @@
+package com.communityServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityServerApplication.class, args);
+	}
+
+}
